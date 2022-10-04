197d57263ea4d1cffa974f4cbfd07cbf1917862b
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-10-04 16:57:16 +0200

    added todo-2

--- src/main/java/com/example/thymeleaf_training/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/example/thymeleaf_training/service/implementation/UserServiceImplementation.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-// TODO Error2
-//@Service
+
+
 public class UserServiceImplementation implements UserService {
     @Override
     public List<User> getUsers() {
